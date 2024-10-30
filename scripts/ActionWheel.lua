101278d4a5811e1b393e5c902bee2f2e5e4a3eab
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-10-30 13:07:38 -0700

    Added sitting anim

--- scripts/ActionWheel.lua
+++ scripts/ActionWheel.lua
@@ -161,6 +161,7 @@ pages.giraffe
 
 -- Animation actions
 pages.anims
+	:action( -1, anims.sitAct)
 	:action( -1, squapi.earsAct)
 	:action( -1, squapi.armsAct)
 	:action( -1, backAct)
--- scripts/Anims.lua
+++ scripts/Anims.lua
@@ -8,6 +8,10 @@ local pose   = require("scripts.Posing")
 -- Animations setup
 local anims = animations.Giraffe
 
+-- Variables
+local canAct  = false
+local canSit  = false
+
 -- Parrot pivots
 local parrots = {
 	
@@ -29,7 +33,21 @@ end
 
 function events.TICK()
 	
+	-- Variables
+	local vel       = player:getVelocity()
+	local sprinting = player:isSprinting()
 	
+	-- Animation states
+	local isAct = anims.sit:isPlaying()
+	
+	-- Animation actions
+	canAct = pose.stand and not(vel:length() ~= 0 or player:getVehicle())
+	canSit = canAct and (not isAct or anims.sit:isPlaying())
+	
+	-- Stop Sit animation
+	if not canSit then
+		anims.sit:stop()
+	end
 	
 end
 
@@ -44,6 +62,7 @@ end
 
 -- GS Blending Setup
 local blendAnims = {
+	{ anim = anims.sit,    ticks = {14,7}  },
 	{ anim = anims.crouch, ticks = {20,20} }
 }
 
@@ -51,3 +70,63 @@ local blendAnims = {
 for _, blend in ipairs(blendAnims) do
 	blend.anim:blendTime(table.unpack(blend.ticks)):onBlend("easeOutQuad")
 end
+
+-- Play sit anim
+function pings.setAnimToggleSit(boolean)
+	
+	anims.sit:playing(canSit and boolean)
+	
+end
+
+-- Host only instructions
+if not host:isHost() then return end
+
+-- Required scripts
+local itemCheck = require("lib.ItemCheck")
+local s, color = pcall(require, "scripts.ColorProperties")
+if not s then color = {} end
+
+-- Sit keybind
+local sitBind   = config:load("AnimSitKeybind") or "key.keyboard.keypad.1"
+local setSitKey = keybinds:newKeybind("Sit Animation"):onPress(function() pings.setAnimToggleSit(not anims.sit:isPlaying()) end):key(sitBind)
+
+-- Keybind updaters
+function events.TICK()
+	
+	local sitKey = setSitKey:getKey()
+	if sitKey ~= sitBind then
+		sitBind = sitKey
+		config:save("AnimSitKeybind", sitKey)
+	end
+	
+end
+
+-- Table setup
+local t = {}
+
+-- Action
+t.sitAct = action_wheel:newAction()
+	:item(itemCheck("scaffolding"))
+	:toggleItem(itemCheck("saddle"))
+	:onToggle(pings.setAnimToggleSit)
+
+-- Update action
+function events.RENDER(delta, context)
+	
+	if action_wheel:isEnabled() then
+		t.sitAct
+			:title(toJson
+				{text = "Play Sit animation", bold = true, color = color.primary}
+			)
+			:toggled(anims.sit:isPlaying())
+		
+		for _, page in pairs(t) do
+			page:hoverColor(color.hover):toggleColor(color.active)
+		end
+		
+	end
+	
+end
+
+-- Returns action
+return t
