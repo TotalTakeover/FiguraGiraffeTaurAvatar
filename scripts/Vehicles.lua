ea38c6d185532876baa15312d507f2edee9651e1
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-12-06 02:32:39 -0700

    Added GSCarrier pivots
    - Added pivot points for users to sit on the avatar at designated spots, up to 2 riders

---
+++ scripts/Vehicles.lua
@@ -0,0 +1,26 @@
+-- Required scripts
+local parts   = require("lib.PartsAPI")
+local carrier = require("lib.GSCarrier")
+
+-- GSCarrier rider
+carrier.rider.addRoots(models)
+carrier.rider.addTag("gscarrier:taur")
+carrier.rider.controller.setGlobalOffset(vec(0, -10, 0))
+carrier.rider.controller.setModifyCamera(false)
+carrier.rider.controller.setModifyEye(false)
+carrier.rider.controller.setAimEnabled(false)
+
+-- GSCarrier vehicle
+carrier.vehicle.addTag("gscarrier:taur", "gscarrier:land")
+
+-- Seat 1
+carrier.vehicle.newSeat("Seat1", parts.group.Seat1, {
+	priority = 2,
+	tags = {["gscarrier:mounted"] = true}
+})
+
+-- Seat 2
+carrier.vehicle.newSeat("Seat2", parts.group.Seat2, {
+	priority = 1,
+	tags = {["gscarrier:mounted"] = true}
+})
